fb9c93fe4750946209db11d4046cb9cb0768b533
Alhafat <vladimir-lukin@inbox.ru>
2023-02-01 22:13:15 +0300

    task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -13,12 +13,13 @@ Console.Clear();
 //  };
 int[,] GetArray ()
 {
-    int[,] matrix = new int[3,3];
+    // int[,] matrix = new int[3,3]
     // {
     //     {1,2,3,},
     //     {4,5,6,},
     //     {7,8,9}
     // };
+    int[,] matrix = new int[3,3];
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
