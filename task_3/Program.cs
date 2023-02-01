8e01066faad9e423bd9d6868cadde1a76030b88d
Alhafat <vladimir-lukin@inbox.ru>
2023-02-01 17:29:03 +0300

    task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача со звездочкой. Напишите программу, которая реализует обход введенного двумерного массива, 
+// начиная с крайнего нижнего левого элемента против часовой стрелки.
+
+// 1 2 3
+// 4 5 6 -> 7 8 9 6 3 2 1 4 5
+// 7 8 9
+
+int[,] matrix = new int[,]
+{
+    {1,2,3,},
+    {4,5,6,},
+    {7,8,9}
+ };
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        // matrix[i,j]=new Random().Next(-5,5);
+
+    }
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
