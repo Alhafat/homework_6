d6b1d1746516dcaae64017fe5711a1546dc21db8
Alhafat <vladimir-lukin@inbox.ru>
2023-02-01 21:01:02 +0300

    task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -5,35 +5,72 @@
 // 4 5 6 -> 7 8 9 6 3 2 1 4 5
 // 7 8 9
 
-int[,] matrix = new int[,]
-{
-    {1,2,3,},
-    {4,5,6,},
-    {7,8,9}
- };
-// for (int i = 0; i < matrix.GetLength(0); i++)
+Console.Clear();
 // {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//         // matrix[i,j]=new Random().Next(-5,5);
-
-//     }
-// }
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//         Console.Write(matrix[i, j] + "\t");
-//     }
-//     Console.WriteLine();
-// }
-
-string text = "";
-for (int i = 2; i < matrix.GetLength(0); i++)
+//     {1,2,3,},
+//     {4,5,6,},
+//     {7,8,9}
+//  };
+int[,] GetArray ()
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int[,] matrix = new int[3,3];
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        text += Convert.ToString(matrix[i, j]) + "\n";
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j]=new Random().Next(-5,5);
+        }
     }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " " + "\t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
 }
-Console.Write(text);
+
+
+string printNumbers(int[,] numbers)
+{
+    string text = "";
+    for (int i = 2; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1)-1; j++)
+        {
+            text += Convert.ToString(numbers[i, j]+" ");
+        }
+    }
+    for (int j = 2; j < numbers.GetLength(1); j++)
+    {
+        for (int i = 2; i > 0; i--)
+        {
+            text += Convert.ToString(numbers[i, j]+" ");
+        }
+    }
+    for (int i = 0; i < numbers.GetLength(0)-2; i++)
+    {
+        for (int j = 2; j >0; j--)
+        {
+            text += Convert.ToString(numbers[i, j]+" ");
+        }
+    }
+    for (int j = 0; j < numbers.GetLength(1)-2; j++)
+    {
+        for (int i = 0; i < numbers.GetLength(1)-1; i++)
+        {
+            text += Convert.ToString(numbers[i, j]+" ");
+        }
+    }
+    text += Convert.ToString(numbers[1, 1]+" ");
+    return text;
+}
+
+
+int[,] numbers=GetArray ();
+string result=printNumbers(numbers);
+
+Console.WriteLine();
+Console.Write(result);
